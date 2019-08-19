69f86ccb8de90f2ab4b3bd7f2966a824c3bbb5a6
Kaynan Silva <kaynan_alves@hotmail.com>
2019-08-19 20:55:10 -0300

    All done with responsiveness

--- test/controllers/questions_controller_test.rb
+++ test/controllers/questions_controller_test.rb
@@ -1,14 +1,7 @@
 require 'test_helper'
 
 class QuestionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get ask" do
-    get questions_ask_url
-    assert_response :success
-  end
-
-  test "should get answer" do
-    get questions_answer_url
-    assert_response :success
-  end
-
+  # test "the truth" do
+  #   assert true
+  # end
 end
